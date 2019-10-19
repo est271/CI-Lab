8c442a0d3ebca04b47579823e564ca0f26c75610
est271 <56150991+est271@users.noreply.github.com>
2019-10-18 18:28:30 -0700

    added unittest

---
+++ test_prime_function.py
@@ -0,0 +1,26 @@
+import unittest
+from prime_ef import find_prime
+
+class Prime_test(unittest.TestCase):
+	"""Input test for find_prime function"""
+
+	def test_not_int(self):
+		"""This method will test if the input is an integer"""
+
+		with self.assertRaises(TypeError):
+			find_prime("str")
+
+		with self.assertRaises(TypeError):
+			find_prime(10.101)
+
+	def test_lessthan_one(self):
+		"""Test if the value inserted is less than or equal to 1"""
+
+		with self.assertRaises(ValueError):
+			find_prime(0)
+
+		with self.assertRaises(ValueError):
+			find_prime(-10)
+
+if __name__ == '__main__':
+	unittest.main()
