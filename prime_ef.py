b39a2249a491ea57f848849073035a25c3a554a9
est271 <56150991+est271@users.noreply.github.com>
2019-10-22 00:21:01 -0700

    Refactored code

--- prime_ef.py
+++ prime_ef.py
@@ -1,23 +1,22 @@
 """Prints prime numbers from 2 up to the number 'num' """
 
-# Remember that the number 1 is not a prime number. prime number has to be divisible only by 1 and itself.
+# Number 1 is not a prime number. prime number has to be divisible only by 1 and itself.
 
 def find_prime(num):
-	"""This function will print all the prime numbers up to the input 'num' """
+    """This function will print all the prime numbers up to the input 'num' """
 
-	if isinstance(num,str) or isinstance(num,float):
-		raise TypeError("number input must be an integer")
+    if not isinstance(num, int) or isinstance(num, bool):
+        raise TypeError("number input must be an integer")
 
-	if num <= 1:
-		raise ValueError("number must be greater than 1")
+    if num <= 1:
+        raise ValueError("number must be greater than 1")
 
-	# The code below will test if every iteration of 'var' is a prime number
-	for var in range(0,num+1):
-	    for var2 in range(2,var+1): # All 'integers' are divisible by 1 so start testing with divisors at 2
-	        if var == var2:         # All options exhausted, the number stored in var is a prime number. print the number
-	            print(var2)
-	            continue
-	        elif (var % var2):      # ex. (5/2= 2 remainder 1 or 2.5) might be a prime number. continue testing
-	            continue
-	        elif (var % var2) == 0: # ex. (4/2= 2 no remainder) Not a prime number, break from loop & test next number
-	            break
+    # The code below will test if every iteration of 'var' is a prime number
+    for var in range(0, num+1):
+        for var2 in range(2, var+1): # All 'integers' are divisible by 1 so start testing with divisors at 2
+            if var == var2:         # All options exhausted, the number stored in var is a prime number. print the number
+                print(var2)
+            elif (var % var2):      # ex. (5/2= 2 remainder 1 or 2.5) might be a prime number. continue testing
+                continue
+            else:                   # ex. (4/2= 2 no remainder) Not a prime number, break from loop & test next number
+                break
--- test_prime_function.py
+++ test_prime_function.py
@@ -1,26 +1,32 @@
+"""Unittest for the prime_ef function"""
+
 import unittest
 from prime_ef import find_prime
 
-class Prime_test(unittest.TestCase):
-	"""Input test for find_prime function"""
+class PrimeTest(unittest.TestCase):
+    """Input test for find_prime function"""
 
-	def test_not_int(self):
-		"""This method will test if the input is an integer"""
+    def test_not_int(self):
+        """This method will test if the input is an integer"""
 
-		with self.assertRaises(TypeError):
-			find_prime("str")
+        with self.assertRaises(TypeError):
+            find_prime("str")
 
-		with self.assertRaises(TypeError):
-			find_prime(10.101)
+        with self.assertRaises(TypeError):
+            find_prime(10.101)
 
-	def test_lessthan_one(self):
-		"""Test if the value inserted is less than or equal to 1"""
+        with self.assertRaises(TypeError):
+            find_prime(True)
 
-		with self.assertRaises(ValueError):
-			find_prime(0)
+    def test_lessthan_one(self):
+        """Test if the value inserted is less than or equal to 1"""
 
-		with self.assertRaises(ValueError):
-			find_prime(-10)
+        with self.assertRaises(ValueError):
+            find_prime(0)
+
+        with self.assertRaises(ValueError):
+            find_prime(-10)
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
+    
